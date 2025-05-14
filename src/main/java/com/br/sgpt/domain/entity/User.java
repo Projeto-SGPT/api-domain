18a4989ccc13656283907a214526f6fc79b7d16d
thalys <thalys@live.com>
2025-05-14 16:20:31 -0300

    fix: user class

--- src/main/java/com/br/sgpt/domain/entity/User.java
+++ src/main/java/com/br/sgpt/domain/entity/User.java
@@ -2,5 +2,24 @@ package com.br.sgpt.domain.entity;
 
 import java.util.UUID;
 
-public record User(UUID id, String username, String password, String email, String role) {
+public class User {
+    private final UUID id;
+    private final String username;
+    private final String password;
+    private final String email;
+    private final String role;
+
+    public User(UUID id, String username, String password, String email, String role) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+    }
+
+    public UUID getId() { return this.id; }
+    public String getUsername() { return this.username; }
+    public String getPassword() { return this.password; }
+    public String getEmail() { return this.email; }
+    public String getRole() { return this.role; }
 }
