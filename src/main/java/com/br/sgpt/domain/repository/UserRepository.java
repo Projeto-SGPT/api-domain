661bcb56128baaf30992449eea6dc036387df887
thalys <thalys@live.com>
2025-05-14 16:35:55 -0300

    fix: remoção retorno no save user

--- src/main/java/com/br/sgpt/domain/repository/UserRepository.java
+++ src/main/java/com/br/sgpt/domain/repository/UserRepository.java
@@ -7,5 +7,5 @@ import java.util.Optional;
 public interface UserRepository {
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
-    User save(User user);
+    void save(User user);
 }
