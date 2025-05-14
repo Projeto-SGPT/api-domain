eb2af84f22455926dfa4262fdd4e6153cf8ffe01
thalys <thalys@live.com>
2025-05-14 16:24:34 -0300

    fix: register email

--- src/main/java/com/br/sgpt/domain/usecases/auth/AuthUseCase.java
+++ src/main/java/com/br/sgpt/domain/usecases/auth/AuthUseCase.java
@@ -2,5 +2,5 @@ package com.br.sgpt.domain.usecases.auth;
 
 public interface AuthUseCase {
     String login(String username, String password);
-    void register(String username, String password, String role);
+    void register(String username, String password, String email, String role);
 }
