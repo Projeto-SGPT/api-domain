752044fb9a238b1e3c8f5bfd010a4afb069fb293
thalys <thalys@live.com>
2025-05-14 16:14:23 -0300

    fix: record user

--- src/main/java/com/br/sgpt/domain/entity/User.java
+++ src/main/java/com/br/sgpt/domain/entity/User.java
@@ -2,21 +2,5 @@ package com.br.sgpt.domain.entity;
 
 import java.util.UUID;
 
-public class User {
-    private UUID id;
-    private String username;
-    private String password;
-    private String role;
-
-    public User(UUID id, String username, String password, String role) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-        this.role = role;
-    }
-
-    public UUID getId() { return id; }
-    public String getUsername() { return username; }
-    public String getPassword() { return password; }
-    public String getRole() { return role; }
+public record User(UUID id, String username, String password, String email, String role) {
 }
